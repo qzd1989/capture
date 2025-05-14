c6aa23d4fceea0c2433eb3f66665ceadd0c6c2bc
kiwi <1145567334@qq.com>
2025-05-14 20:03:20 +0800

    1

--- src/engine/macos.rs
+++ src/engine/macos.rs
@@ -1,4 +1,4 @@
-use super::{FrameHandler, bgra_to_rgba};
+use super::FrameHandler;
 use crate::Config;
 use crate::Format;
 use crate::Frame;
@@ -140,5 +140,26 @@ impl<T: FrameHandler> Engine<T> {
     }
     pub fn stop(&self) {
         self.status.store(false, Ordering::Relaxed);
+    }
+}
+
+fn bgra_to_rgba(bgra_data: &[u8], rgba_data: &mut Vec<u8>) {
+    let len = bgra_data.len();
+    assert!(len % 4 == 0);
+    if rgba_data.capacity() < len {
+        rgba_data.reserve_exact(len - rgba_data.capacity());
+    }
+    unsafe {
+        rgba_data.set_len(len);
+        let src = bgra_data.as_ptr();
+        let dst = rgba_data.as_mut_ptr();
+        let mut i = 0;
+        while i < len {
+            *dst.add(i) = *src.add(i + 2); // R
+            *dst.add(i + 1) = *src.add(i + 1); // G
+            *dst.add(i + 2) = *src.add(i); // B
+            *dst.add(i + 3) = *src.add(i + 3); // A
+            i += 4;
+        }
     }
 }
--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -13,24 +13,3 @@ pub use engine::*;
 
 pub trait FrameHandler: Fn(Frame, u32) + Send + Sync + 'static {}
 impl<T> FrameHandler for T where T: Fn(Frame, u32) + Send + Sync + 'static {}
-#[allow(dead_code)]
-pub fn bgra_to_rgba(bgra_data: &[u8], rgba_data: &mut Vec<u8>) {
-    let len = bgra_data.len();
-    assert!(len % 4 == 0);
-    if rgba_data.capacity() < len {
-        rgba_data.reserve_exact(len - rgba_data.capacity());
-    }
-    unsafe {
-        rgba_data.set_len(len);
-        let src = bgra_data.as_ptr();
-        let dst = rgba_data.as_mut_ptr();
-        let mut i = 0;
-        while i < len {
-            *dst.add(i) = *src.add(i + 2); // R
-            *dst.add(i + 1) = *src.add(i + 1); // G
-            *dst.add(i + 2) = *src.add(i); // B
-            *dst.add(i + 3) = *src.add(i + 3); // A
-            i += 4;
-        }
-    }
-}
