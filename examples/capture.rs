dd050bf0b8e668e710a39e090ff512a7645a127c
kiwi <1145567334@qq.com>
2025-05-15 02:25:15 +0800

    1

--- examples/capture.rs
+++ examples/capture.rs
@@ -2,8 +2,6 @@ use capture::{Capturer, Config, Format};
 fn main() {
     let config = Config::new(Format::BGRA);
     let mut frame = Capturer::screenshot(config).unwrap();
-    dbg!(frame.width, frame.height);
-
     let image_buffer = frame.to_image_buffer_rgba8().unwrap();
     image_buffer.save("a.png").unwrap();
 }
